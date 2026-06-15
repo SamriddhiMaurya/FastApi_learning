ad9e2dd51228ec36249be0d0cb14851b4fec3e8c
SamriddhiMaurya <samriddhimaurya9005@gmail.com>
2026-06-15 17:46:45 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel
+app = FastAPI()
+
+# @app.get("/")
+# def home():
+#     return {"message": "Hello World"}
+
+
+
+#dynamic routes
+
+@app.get("/items/{item_id}")
+def items(item_id):
+    return {"itemm_id": item_id}
+
+users=[]
+#pydantic models
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+ 
+ 
+    
+@app.post("/create_users")
+def create_user(user: User):
+    users.append(user)
+    return {"message": "User created successfully!", "data": user}
+
+@app.get("/get_users")
+def get_users():
+    return {"message": "List of users", "data": users}
+
+
+class Userr(BaseModel):
+    name: str
+    age: int
+    passkey: int
+
+
+#response model
+
+class UserResponse(BaseModel):
+    name: str
+    age: int
+    
+@app.get("/new_user", response_model=UserResponse)
+def get_user():
+    return {
+        "name": "John Doe",
+        "age": 30,      
+        "passkey": 12345
+    }
+    
+
+
+
+#status code and responses
+
