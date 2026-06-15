bc999dbdaa2f3e6539919e4ae568e3762c02ea90
SamriddhiMaurya <samriddhimaurya9005@gmail.com>
2026-06-15 19:38:58 +0530

    global exception handller

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI , HTTPException, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 app = FastAPI()
 
@@ -58,3 +59,34 @@ def get_user():
 
 #status code and responses
 
+#exceptional handelling 
+
+
+
+#custom exception
+class UserNotFoundException(Exception):
+    def __init__(self, name:str):
+        self.name = name
+        
+        
+@app.exception_handler(UserNotFoundException)
+def user_not_found_handeller(request:Request, exc:UserNotFoundException ):
+    return JSONResponse(
+        status_code=404, 
+        content={
+            "status":"error", 
+            "message":f"User {exc.name} not found"
+        }
+    )
+    
+
+@app.get("/user/{name}")
+def get_user(name:str):
+    if name != "samriddhi":
+        raise UserNotFoundException(name)
+    return{
+        "name":name
+    }
+    
+    
+    
