57605c9918e0bf933181a726b09e88b2b5d53929
SamriddhiMaurya <samriddhimaurya9005@gmail.com>
2026-06-23 19:35:25 +0530

    sql-alchemy

--- main.py
+++ main.py
@@ -1,136 +1,72 @@
-from fastapi import FastAPI , Depends, Header  , HTTPException , Request #header for auth
-from pydantic import BaseModel
-import time #for middleware
-app = FastAPI()
+import sqlite3
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from sqlalchemy import Column, Integer, String
 
-# @app.get("/")
-# def home():
-#     return {"message": "Hello World"}
+app=FastAPI()
+conn =  sqlite3.connect("test.db" , check_same_thread= False)
 
+cursor = conn.cursor() 
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    completed TEXT
+    )             
+""")
 
-#dynamic routes
-
-@app.get("/items/{item_id}")
-def items(item_id):
-    return {"itemm_id": item_id}
-
-users=[]
-#pydantic models
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
- 
- 
-    
-@app.post("/create_users")
-def create_user(user: User):
-    users.append(user)
-    return {"message": "User created successfully!", "data": user}
-
-@app.get("/get_users")
-def get_users():
-    return {"message": "List of users", "data": users}
+conn.commit()  #to save the changes 
 
 
-class Userr(BaseModel):
-    name: str
-    age: int
-    passkey: int
-
-
-#response model
-
-class UserResponse(BaseModel):
-    name: str
-    age: int
-    
-@app.get("/new_user", response_model=UserResponse)
-def get_user():
-    return {
-        "name": "John Doe",
-        "age": 30,      
-        "passkey": 12345
-    }
-    
-
-
-
-
-
-#dependency injection 
-#commom logic
-
-def common_logic():
+@app.get("/")
+def home():
     return{
-        "message":"common logic executed"
-    } 
+        "message":"SQLLite connected."
+    }
+    
+    
+    
+#SQLAlchemy Database
 
 
-@app.get("/home")
-def homee(data=Depends(common_logic)):
-    return data
+DATABASE_URL = "sqlite:///./test.db"
 
 
-#reusable logic of dependency injection 
+#databse connection using engine
+engine = create_engine(
+    DATABASE_URL, 
+    connect_args={"check_same_thread":False}
+)
 
-def get_current_user():
+#sessionLocal helps db to know that operitaions can be performed in db
+sessionLocal = sessionmaker(bind=engine)
+
+
+#base is used to act as a base for model
+Base = declarative_base()
+
+
+class Todo(Base):
+    __tablename__="todos"
+    
+    id= Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    completed = Column(String)
+    
+Base.metadata.create_all(bind = engine)
+
+def get_db():
+    db=sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@app.get("/")
+def home(db:Session = Depends(get_db)):
     return{
-        "user": "Sam"
+        "message":"DB connected file"
     }
-    
-@app.get("/profile")
-def profile(user = Depends(get_current_user)):
-    return user 
 
-@app.get("/dashboard")
-def dashboard(user = Depends(get_current_user)):
-    return user 
-
-
-
-#Auth
-
-
-def verify_token(token: str = Header(None)):
-    if token != "mysecrettoken":
-        raise HTTPException(
-            status_code=401,
-            detail="unauthorized"
-        )
-    return {
-        "user": "Authorized User"
-    }
-    
-@app.get("/secure-data")
-def secure_data(user=Depends(verify_token)):
-    return {
-        "message": "secure data accessed",
-        "user": user
-    }
-    
-    
-    
-#middleware 
-
-
-@app.middleware("http")
-# async def my_middleware(request: Request, call_next):
-#     print("Request Received")
-#     response= await call_next(request)
-#     print("response sent")
-#     return response
-
-
-@app.middleware("http")
-async def log_middleware(request: Request, call_next):
-    start_time=time.time()
-    
-    response = await call_next(request)
-    
-    process_time = time.time()-start_time
-    
-    print(f"Path:{request.url.path} | Time:{process_time}")
-    
-    return response
